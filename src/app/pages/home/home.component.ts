e46f9721191732a0a908e3b17e6bf6a4bebc7b42
Youssef Ben Abdessalem <djoabdo1999@gmail.com>
2023-12-27 03:28:35 +0100

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ArrayType} from "@angular/compiler";
+import {HttpClient} from "@angular/common/http";
 
-interface Anime {
+interface AnimeGallery {
   url: string;
   name: string;
   subTitle: string;
   gender: string[];
+}
+
+interface Anime {
+  id: number;
+  name: string;
+  opened:bigint;
+  description: string;
+  image: string;
+  genders: string[];
+  comments: any[]; // You might want to replace "any" with the actual type of your comments
+  favoritedBy: any[]; // You might want to replace "any" with the actual type of users
+  ratings: any[]; // You might want to replace "any" with the actual type of ratings
 }
 
 @Component({
@@ -13,9 +26,33 @@ interface Anime {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
-  animeList: Anime[] = [
+  animeList: Anime[] = [];
+  BestFive: Anime[] = [];
+
+  ngOnInit() {
+    this.getAnime();
+    this.getBestFive();
+  }
+
+  getAnime() {
+    this.http.get<Anime[]>("http://localhost:8080/anime/all").subscribe((res: Anime[]) => {
+      this.animeList = res;
+    });
+  }
+
+  getBestFive(){
+    this.http.get<Anime[]>("http://localhost:8080/anime/top5").subscribe((res: Anime[]) => {
+      this.BestFive = res;
+    });
+  }
+
+
+  constructor(private http:HttpClient) {
+  }
+
+  Gallery: AnimeGallery[] = [
     {
       url: 'https://s.yimg.com/ny/api/res/1.2/LKeXpfVTBDB5YaWjtYBlWg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://media.zenfs.com/en/comingsoon_net_477/5e257a80196052d3e53e2cac481eaed9',
       name: 'One Piece',
@@ -39,11 +76,11 @@ export class HomeComponent {
   currentIndex = 0;
 
   showPrev() {
-    this.currentIndex = (this.currentIndex - 1 + this.animeList.length) % this.animeList.length;
+    this.currentIndex = (this.currentIndex - 1 + this.Gallery.length) % this.Gallery.length;
   }
 
   showNext() {
-    this.currentIndex = (this.currentIndex + 1) % this.animeList.length;
+    this.currentIndex = (this.currentIndex + 1) % this.Gallery.length;
   }
 
 }
